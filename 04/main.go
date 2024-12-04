c1eaad7e4e4dce5d0305b06939f418f0c012cf1f
CloudyShoe <cloudyshoe@gmail.com>
2024-12-04 02:12:02 -0500

    day 4 part one

---
+++ 04/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func PartOne(input []string) int {
+	result := 0
+	rows := len(input)
+	cols := len(input[0])
+
+	for i := range input {
+		for j := range input[i] {
+			if input[i][j] == 'X' {
+				if j-3 >= 0 {
+					if input[i][j-3:j] == "SAM" {
+						result++
+					}
+				}
+				if j+4 <= cols {
+					if input[i][j+1:j+4] == "MAS" {
+						result++
+					}
+				}
+				if i-3 >= 0 {
+					if input[i-1][j] == 'M' && input[i-2][j] == 'A' && input[i-3][j] == 'S' {
+						result++
+					}
+				}
+				if i+3 < rows {
+					if input[i+1][j] == 'M' && input[i+2][j] == 'A' && input[i+3][j] == 'S' {
+						result++
+					}
+				}
+				if i-3 >= 0 && j-3 >= 0 {
+					if input[i-1][j-1] == 'M' && input[i-2][j-2] == 'A' && input[i-3][j-3] == 'S' {
+						result++
+					}
+				}
+				if i-3 >= 0 && j+3 < cols {
+					if input[i-1][j+1] == 'M' && input[i-2][j+2] == 'A' && input[i-3][j+3] == 'S' {
+						result++
+					}
+				}
+				if i+3 < rows && j-3 >= 0 {
+					if input[i+1][j-1] == 'M' && input[i+2][j-2] == 'A' && input[i+3][j-3] == 'S' {
+						result++
+					}
+				}
+				if i+3 < rows && j+3 < cols {
+					if input[i+1][j+1] == 'M' && input[i+2][j+2] == 'A' && input[i+3][j+3] == 'S' {
+						result++
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
+func PartTwo(input []string) int {
+	result := 0
+
+	return result
+}
+
+func main() {
+	inputFile, _ := os.ReadFile("input.txt")
+	input := strings.Split(string(inputFile), "\n")
+
+	partOneResult := PartOne(input)
+	fmt.Println("Part One Result:", partOneResult)
+
+	partTwoResult := PartTwo(input)
+	fmt.Println("Part Two Result:", partTwoResult)
+}
