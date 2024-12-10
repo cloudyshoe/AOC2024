0edc4479a9215bbe7c0af5a402b02981f0fdbced
CloudyShoe <cloudyshoe@gmail.com>
2024-12-10 16:25:58 -0500

    day 9 presize slices

--- 09/main.go
+++ 09/main.go
@@ -9,12 +9,24 @@ import (
 
 func PartOne(input []string) int {
 	result := 0
-	var blocks []int
-	var freeSpace []int
-	var usedSpace []int
+	//var blocks []int
+	//var freeSpace []int
+	//var usedSpace []int
 
+	size := 0
+
+	tmp := make([]int, len(input[0]))
 	for i, char := range input[0] {
 		num := utils.Atoi(char)
+		tmp[i] = num
+		size += num
+	}
+
+	blocks := make([]int, 0, size)
+	freeSpace := make([]int, 0, size)
+	usedSpace := make([]int, 0, size)
+
+	for i, num := range tmp {
 		if i%2 == 0 {
 			for j := 0; j < num; j++ {
 				blocks = append(blocks, i/2)
@@ -57,12 +69,24 @@ func PartTwo(input []string) int {
 		len   int
 	}
 
-	var blocks []int
-	var freeSpace []Block
-	var usedSpace []Block
+	//var blocks []int
+	//var freeSpace []Block
+	//var usedSpace []Block
 
+	size := 0
+
+	tmp := make([]int, len(input[0]))
 	for i, char := range input[0] {
 		num := utils.Atoi(char)
+		tmp[i] = num
+		size += num
+	}
+
+	blocks := make([]int, 0, size)
+	freeSpace := make([]Block, 0, size)
+	usedSpace := make([]Block, 0, size)
+
+	for i, num := range tmp {
 		if i%2 == 0 {
 			usedSpace = append(usedSpace, Block{start: len(blocks), len: num})
 			for j := 0; j < num; j++ {
