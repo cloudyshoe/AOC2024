382725f7a1dec2759a7a0cde3aef9a9ff9dcf06d
CloudyShoe <cloudyshoe@gmail.com>
2024-12-13 07:05:04 -0500

    day 13 part 1

--- 13/main.go
+++ 13/main.go
@@ -1,30 +1,74 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
-func PartOne(input []string) int  {
-    result := 0
+type Button struct {
+	dx int
+	dy int
+}
 
-    return result
+type Prize struct {
+	x int
+	y int
+}
+
+func PartOne(input []string) int {
+	result := 0
+	aButtons := make([]Button, 0, len(input)/4)
+	bButtons := make([]Button, 0, len(input)/4)
+	prizes := make([]Prize, 0, len(input)/4)
+
+	for _, group := range input {
+		entries := strings.Split(group, "\n")
+		buttonA := Button{}
+		fmt.Sscanf(entries[0], "Button A: X+%d, Y+%d", &buttonA.dx, &buttonA.dy)
+		aButtons = append(aButtons, buttonA)
+		buttonB := Button{}
+		fmt.Sscanf(entries[1], "Button B: X+%d, Y+%d", &buttonB.dx, &buttonB.dy)
+		bButtons = append(bButtons, buttonB)
+		prize := Prize{}
+		fmt.Sscanf(entries[2], "Prize: X=%d, Y=%d", &prize.x, &prize.y)
+		prizes = append(prizes, prize)
+	}
+
+	for i := 0; i < len(aButtons); i++ {
+		minTokens := 1000
+		for a := 0; a <= 100; a++ {
+			for b := 0; b <= 100; b++ {
+				if (aButtons[i].dx*a+bButtons[i].dx*b) == prizes[i].x &&
+					(aButtons[i].dy*a+bButtons[i].dy*b) == prizes[i].y {
+					tokensUsed := 3*a + b
+					if tokensUsed < minTokens {
+						minTokens = tokensUsed
+					}
+				}
+			}
+		}
+		if minTokens < 1000 {
+			result += minTokens
+		}
+	}
+
+	return result
 }
 
 func PartTwo(input []string) int {
-    result := 0
+	result := 0
 
-    return result
+	return result
 }
 
-func main () {
-    inputFile, _ := os.ReadFile("input.txt")
-    input := strings.Split(string(inputFile), "\n")
+func main() {
+	inputFile, _ := os.ReadFile("input.txt")
+	input := strings.Split(string(inputFile), "\n\n")
 
-    partOneResult := PartOne(input)
-    fmt.Println("Part One Result:", partOneResult)
+	partOneResult := PartOne(input)
+	fmt.Println("Part One Result:", partOneResult)
 
-    partTwoResult := PartTwo(input)
-    fmt.Println("Part Two Result:", partTwoResult)
+	partTwoResult := PartTwo(input)
+	fmt.Println("Part Two Result:", partTwoResult)
 }
--- 13/main_test.go
+++ 13/main_test.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-    "testing"
-    "os"
-    "strings"
+	"os"
+	"strings"
+	"testing"
 )
 
 func TestPartOne(t *testing.T) {
-    inputFile, _ := os.ReadFile("example.txt")
-    input := strings.Split(string(inputFile), "\n")
-    want := PartOne(input)
+	inputFile, _ := os.ReadFile("example.txt")
+	input := strings.Split(string(inputFile), "\n\n")
+	want := PartOne(input)
 
-    got := PartOne(input)
+	got := PartOne(input)
 
-    if got != want {
-        t.Errorf("expected '%d' but got '%d'", want, got)
-    }
+	if got != want {
+		t.Errorf("expected '%d' but got '%d'", want, got)
+	}
 }
 
 func TestPartTwo(t *testing.T) {
-    inputFile, _ := os.ReadFile("example.txt")
-    input := strings.Split(string(inputFile), "\n")
-    want := PartTwo(input)
+	inputFile, _ := os.ReadFile("example.txt")
+	input := strings.Split(string(inputFile), "\n\n")
+	want := PartTwo(input)
 
-    got := PartTwo(input)
+	got := PartTwo(input)
 
-    if got != want {
-        t.Errorf("expected '%d' but got '%d'", want, got)
-    }
+	if got != want {
+		t.Errorf("expected '%d' but got '%d'", want, got)
+	}
 }
 
 func BenchmarkPartOne(b *testing.B) {
-    inputFile, _ := os.ReadFile("input.txt")
-    input := strings.Split(string(inputFile), "\n")
+	inputFile, _ := os.ReadFile("input.txt")
+	input := strings.Split(string(inputFile), "\n\n")
 
-    for i := 0; i < b.N; i++ {
-        PartOne(input)
-    }
+	for i := 0; i < b.N; i++ {
+		PartOne(input)
+	}
 }
 
 func BenchmarkPartTwo(b *testing.B) {
-    inputFile, _ := os.ReadFile("input.txt")
-    input := strings.Split(string(inputFile), "\n")
+	inputFile, _ := os.ReadFile("input.txt")
+	input := strings.Split(string(inputFile), "\n\n")
 
-    for i := 0; i < b.N; i++ {
-        PartTwo(input)
-    }
+	for i := 0; i < b.N; i++ {
+		PartTwo(input)
+	}
 }
