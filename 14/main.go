5390c5c3c35755388302f85002ba72601abcc9b0
CloudyShoe <cloudyshoe@gmail.com>
2024-12-14 02:48:19 -0500

    Day 14 Part 1

---
+++ 14/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"aoc/utils"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var debug *bool = flag.Bool("debug", false, "Print debug statements")
+
+type Robot struct {
+	loc     utils.Coord
+	vel     utils.Coord
+	counted bool
+}
+
+func PartOne(input []string, maxRows int, maxCols int) int {
+	result := 1
+	robots := make([]Robot, len(input))
+
+	for i, line := range input {
+		robot := Robot{}
+		fmt.Sscanf(line, "p=%d,%d v=%d,%d", &robot.loc.Col, &robot.loc.Row, &robot.vel.Col, &robot.vel.Row)
+		seconds := 0
+		for seconds < 100 {
+			robot.loc = robot.loc.Add(robot.vel)
+			if robot.loc.Col < 0 {
+				robot.loc.Col = maxCols + robot.loc.Col
+			}
+			if robot.loc.Col >= maxCols {
+				robot.loc.Col = robot.loc.Col - maxCols
+			}
+			if robot.loc.Row < 0 {
+				robot.loc.Row = maxRows + robot.loc.Row
+			}
+			if robot.loc.Row >= maxRows {
+				robot.loc.Row = robot.loc.Row - maxRows
+			}
+			seconds++
+		}
+		robots[i] = robot
+		if *debug {
+			fmt.Println(robot)
+		}
+	}
+
+	quadrants := []utils.Bounds{
+		{Min: utils.Coord{Row: 0, Col: 0}, Max: utils.Coord{Row: maxRows / 2, Col: maxCols / 2}},
+		{Min: utils.Coord{Row: 0, Col: maxCols/2 + 1}, Max: utils.Coord{Row: maxRows / 2, Col: maxCols}},
+		{Min: utils.Coord{Row: maxRows/2 + 1, Col: 0}, Max: utils.Coord{Row: maxRows, Col: maxCols / 2}},
+		{Min: utils.Coord{Row: maxRows/2 + 1, Col: maxCols/2 + 1}, Max: utils.Coord{Row: maxRows, Col: maxCols}},
+	}
+
+	for _, quandrant := range quadrants {
+		if *debug {
+			fmt.Println(quandrant)
+		}
+		count := 0
+		for _, robot := range robots {
+			if !robot.counted {
+				if robot.loc.In(quandrant) {
+					count++
+					robot.counted = true
+				}
+
+			}
+		}
+		result *= count
+	}
+
+	if *debug {
+		for row := 0; row < maxRows; row++ {
+			for col := 0; col < maxCols; col++ {
+				found := false
+				for _, robot := range robots {
+					if robot.loc.Row == row && robot.loc.Col == col {
+						fmt.Print("#")
+						found = true
+						break
+					}
+				}
+				if !found {
+					fmt.Print(".")
+				}
+			}
+			fmt.Print("\n")
+		}
+	}
+
+	return result
+}
+
+func PartTwo(input []string) int {
+	result := 0
+
+	return result
+}
+
+func main() {
+
+	flag.Parse()
+
+	inputFile, _ := os.ReadFile("input.txt")
+	input := strings.Split(string(inputFile), "\n")
+
+	partOneResult := PartOne(input, 103, 101)
+	fmt.Println("Part One Result:", partOneResult)
+
+	partTwoResult := PartTwo(input)
+	fmt.Println("Part Two Result:", partTwoResult)
+}
