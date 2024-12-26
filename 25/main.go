d9235d783c7743795671a76f4ea960077aca9d3a
CloudyShoe <cloudyshoe@gmail.com>
2024-12-26 12:37:37 -0500

    move counting to a function

--- 25/main.go
+++ 25/main.go
@@ -9,32 +9,60 @@ import (
 
 var debug *bool = flag.Bool("debug", false, "Print debug statements")
 
+type Pins struct {
+	lockOrKey string
+	heights   [5]int
+}
+
+func countPins(schematic string) (pins Pins) {
+	lines := strings.Split(schematic, "\n")
+	var startRow, endRow int
+	if lines[0][0] == '#' {
+		pins.lockOrKey = "lock"
+		startRow = 1
+		endRow = len(lines)
+	} else {
+		pins.lockOrKey = "key"
+		startRow = 0
+		endRow = len(lines) - 1
+	}
+	for row := startRow; row < endRow; row++ {
+		for col := 0; col < len(lines[row]); col++ {
+			if lines[row][col] == '#' {
+				pins.heights[col]++
+			}
+		}
+	}
+	return
+}
+
+func checkFit(key, lock [5]int) int {
+	fit := true
+	for i := range key {
+		if key[i]+lock[i] > 5 {
+			fit = false
+			break
+		}
+	}
+	if fit {
+		return 1
+	} else {
+		return 0
+	}
+
+}
+
 func PartOne(input []string) int {
 	result := 0
 	locks := make([][5]int, 0, len(input)/2)
 	keys := make([][5]int, 0, len(input)/2)
 
-	for _, schematics := range input {
-		lines := strings.Split(schematics, "\n")
-		var heights [5]int
-		if lines[0][0] == '#' {
-			for row := 1; row < len(lines); row++ {
-				for col := 0; col < len(lines[row]); col++ {
-					if lines[row][col] == '#' {
-						heights[col]++
-					}
-				}
-			}
-			locks = append(locks, heights)
+	for _, schematic := range input {
+		pins := countPins(schematic)
+		if pins.lockOrKey == "lock" {
+			locks = append(locks, pins.heights)
 		} else {
-			for row := 0; row < len(lines)-1; row++ {
-				for col := 0; col < len(lines[row]); col++ {
-					if lines[row][col] == '#' {
-						heights[col]++
-					}
-				}
-			}
-			keys = append(keys, heights)
+			keys = append(keys, pins.heights)
 		}
 	}
 
@@ -45,16 +73,7 @@ func PartOne(input []string) int {
 
 	for _, key := range keys {
 		for _, lock := range locks {
-			fit := true
-			for i := range key {
-				if key[i]+lock[i] > 5 {
-					fit = false
-					break
-				}
-			}
-			if fit {
-				result += 1
-			}
+			result += checkFit(key, lock)
 		}
 	}
 
